127f96755acef7b5f3bb8ff0029babf0704c3deb
Kyle Robinson <kylerobbo12@gmail.com>
2022-01-01 18:19:05 +0400

    install react and main dependencies

---
+++ src/containers/Editor.js
@@ -0,0 +1,12 @@
+
+const Editor = () => {
+    return (
+        <>
+            <div id="gjs">
+                <h1>Hello World Component</h1>
+            </div>
+        </>
+    )
+}
+
+export default Editor;
